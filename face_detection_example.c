2cf92ddae16336bcc4e9b3dfcd2e37b982c0d03d
Pete Warden <pete@petewarden.com>
2022-10-04 17:23:46 -0700

    Updated to latest API

--- face_detection_example.c
+++ face_detection_example.c
@@ -1,5 +1,12 @@
 // Copyright (c) 2022 Useful Sensors Inc.
 // SPDX-License-Identifier: Apache-2.0
+//
+// Example code to communicate with a Useful Sensors' Person Sensor.
+// See the full developer guide at https://usfl.ink/ps_dev for more information.
+//   
+// Demonstrates how to set up and receive information from a person sensor. 
+// Canonical home for this code is at 
+// https://github.com/usefulsensors/useful_pico_person_sensor
 
 #include <stdio.h>
 #include <string.h>
@@ -9,19 +16,7 @@
 
 #include "hardware/i2c.h"
 
-// Example code to communicate with a Useful Sensors' Person Sensor
-//   
-// Demonstrates how to set up and receive information from a person sensor. 
-// Canonical home for this code is at 
-// https://github.com/usefulsensors/useful_pico_person_sensor
-
-// GPIO PICO_DEFAULT_I2C_SDA_PIN (On Pico this is GP4 (physical pin 6)) -> SDA of person sensor
-// GPIO PICO_DEFAULT_I2C_SCK_PIN (On Pico this is GP5 (physcial pin 7)) -> SCL of person sensor
-// Vsys (physical pin 39) -> VDD on person sensor
-// GND (physical pin 38)  -> GND on person sensor
-
-// The I2C address of the person sensor board.
-const uint8_t PERIPHERAL_ADDRESS = 0x62;
+#include "person_sensor.h"
 
 // Speed of the I2C bus. The sensor supports many rates but 400KHz works.
 const int32_t I2C_BAUD_RATE = (400 * 1000);
@@ -30,29 +25,6 @@ const int32_t I2C_BAUD_RATE = (400 * 1000);
 // frequently as you like, but the results only change at about 5FPS, so
 // waiting for 200ms is reasonable.
 const int32_t SAMPLE_DELAY_MS = 200;
-
-// Configuration commands for the sensor. Write this as a byte to the I2C bus
-// followed by a second byte as an argument value.
-const uint8_t REG_MODE = 0x01;
-const uint8_t REG_SINGLE_SHOT = 0x02;
-const uint8_t REG_ENABLE_ID = 0x03;
-const uint8_t REG_LABEL_ID = 0x04;
-const uint8_t REG_ENABLE_SMOOTHING = 0x05;
-
-// This is the structure of the packet returned over the wire from the sensor
-// when we do an I2C read from the peripheral address. The C standard doesn't
-// guarantee the byte-wise layout of a regular struct across different
-// platforms, so we add the non-standard (but widely supported) __packed__
-// attribute to ensure the layout is the same as the wire representation.
-typedef struct __attribute__ ((__packed__)) {
-  float confidence;     // Bytes 0 to 3
-  float id_confidence;  // Bytes 4 to 7
-  uint8_t x1;           // Byte 8
-  uint8_t y1;           // Byte 9
-  uint8_t x2;           // Byte 10
-  uint8_t y2;           // Byte 11
-  int8_t identity;      // Byte 12
-} inference_results_t;
 
 // I2C reserves some addresses for special purposes. We exclude these from the scan.
 // These are any addresses of the form 000 0xxx or 111 1xxx
@@ -69,10 +41,9 @@ void scan_i2c_bus() {
       printf("%02x ", addr);
     }
 
-    // Perform a 1-byte dummy read from the probe address. If a slave
+    // Perform a 1-byte dummy read from the probe address. If a peripheral
     // acknowledges this address, the function returns the number of bytes
-    // transferred. If the address byte is ignored, the function returns
-    // -1.
+    // transferred. If the address byte is ignored, the function returns -1.
 
     // Skip over any reserved addresses.
     int ret;
@@ -93,39 +64,45 @@ int main() {
 
     printf("Setting up i2c\n");
 
-    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+    // Use I2C0 on the default SDA and SCL pins (6, 7 on a Pico).
     i2c_init(i2c_default, I2C_BAUD_RATE);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
     // Make the I2C pins available to picotool.
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, 
+      PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
 
     printf("Setup done for i2c\n");
 
-    inference_results_t results;
+    person_sensor_results_t results;
     while (1) {
         // Perform a read action on the I2C address of the sensor to get the
         // current face information detected.
         int num_bytes_read = i2c_read_blocking(
             i2c_default,
-            PERIPHERAL_ADDRESS, 
+            PERSON_SENSOR_I2C_ADDRESS, 
             (uint8_t*)(&results), 
-            sizeof(inference_results_t), 
+            sizeof(person_sensor_results_t), 
             false);
         // If there's no result from the sensor, don't print anything after the
         // first time.
-        if (num_bytes_read != sizeof(inference_results_t)) {
+        if (num_bytes_read != sizeof(person_sensor_results_t)) {
             printf("No person sensor results found on the i2c bus\n");
             sleep_ms(SAMPLE_DELAY_MS);
             continue;
         }
 
-        printf("Face confidence %f\% bbox [%d %d %d %d], id conf %f\% id %d\n", 
-            results.confidence, 
-            results.x1, results.y1, results.x2, results.y2, 
-            results.id_confidence, results.identity);
+        printf("********\n");
+        printf("%d faces found\n", results.num_faces);
+        for (int i = 0; i < results.num_faces; ++i) {
+            const person_sensor_face_t* face = &results.faces[i];
+            printf("Face #%d: %d confidence, (%d, %d), %dx%d, %s\n",
+              i, face->box_confidence, face->box_left, face->box_top,
+              face->box_width, face->box_height, 
+              face->is_facing ? "facing" : "not facing");
+        }
 
         sleep_ms(SAMPLE_DELAY_MS);
     }
---
+++ person_sensor.h
@@ -0,0 +1,67 @@
+#ifndef INCLUDE_PERSON_SENSOR_H
+#define INCLUDE_PERSON_SENSOR_H
+
+#include <stdint.h>
+
+// The I2C address of the person sensor board.
+const uint8_t PERSON_SENSOR_I2C_ADDRESS = 0x62;
+
+// Configuration commands for the sensor. Write this as a byte to the I2C bus
+// followed by a second byte as an argument value.
+const uint8_t REG_MODE = 0x01;
+const uint8_t REG_SINGLE_SHOT = 0x02;
+const uint8_t REG_ENABLE_ID = 0x03;
+const uint8_t REG_LABEL_ID = 0x04;
+const uint8_t REG_ENABLE_SMOOTHING = 0x05;
+
+// The person sensor will never output more than four faces.
+#define MAX_FACES_COUNT (4)
+
+// The following structures represent the data format returned from the person
+// sensor over the I2C communication protocol. The C standard doesn't
+// guarantee the byte-wise layout of a regular struct across different
+// platforms, so we add the non-standard (but widely supported) __packed__
+// attribute to ensure the layouts are the same as the wire representation.
+
+// The results returned from the sensor have a short header providing
+// information about the length of the data packet:
+//   reserved: Currently unused bytes.
+//   data_size: Length of the entire packet, excluding the header and checksum.
+//     For version 1.0 of the sensor, this should be 40.
+typedef struct __attribute__ ((__packed__)) {
+    uint8_t reserved[2];  // Bytes 0 to 1.
+    uint16_t data_size;   // Bytes 2 to 3.
+} person_sensor_results_header_t;
+
+// Each face found has a set of information associated with it:
+//   box_confidence: How certain we are we have found a face, from 0 to 255.
+//   box_left: X coordinate of the left side of the box, from 0 to 255.
+//   box_top: Y coordinate of the top edge of the box, from 0 to 255.
+//   box_width: Width of the box, where 255 is the full view port size.
+//   box_height: Height of the box, where 255 is the full view port size.
+//   id_confidence: How sure the sensor is about the recognition result.
+//   id: Numerical ID assigned to this face.
+//   is_looking_at: Whether the person is facing the camera, 0 or 1.
+typedef struct __attribute__ ((__packed__)) {
+    uint8_t box_confidence;   // Byte 1.
+    uint8_t box_left;         // Byte 2.
+    uint8_t box_top;          // Byte 3.
+    uint8_t box_width;        // Byte 4.
+    uint8_t box_height;       // Byte 5.
+    uint8_t id_confidence;    // Byte 6.
+    int8_t id;                // Byte 7
+    uint8_t is_facing;        // Byte 8.
+} person_sensor_face_t;
+
+// This is the full structure of the packet returned over the wire from the
+// sensor when we do an I2C read from the peripheral address.
+// The checksum should be the CRC16 of bytes 0 to 38. You shouldn't need to
+// verify this in practice, but we found it useful during our own debugging.
+typedef struct __attribute__ ((__packed__)) {
+    person_sensor_results_header_t header;        // Bytes 0 to 4.
+    int8_t num_faces;                             // Byte 5.
+    person_sensor_face_t faces[MAX_FACES_COUNT];  // Bytes 6 to 37.
+    uint16_t checksum;                            // Bytes 38 to 39.
+} person_sensor_results_t;
+
+#endif  // INCLUDE_PERSON_SENSOR_H
